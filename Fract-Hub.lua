516a8c6f04bb73cd7c61c5b837cd652e88214682
xFract <adomensdo@gmail.com>
2026-01-13 15:45:34 +0900

    Update Fract-Hub.lua

--- Fract-Hub.lua
+++ Fract-Hub.lua
@@ -5,5 +5,6 @@
 local GameID = game.GameId
 
 if GameID == 7798947148 then
-    loadstring(game:HttpGet(""))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xFract/Fract-Hub/refs/heads/main/AFQ.lua"))()
 end
+

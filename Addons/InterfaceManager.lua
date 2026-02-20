a0f075620b0dc0938756aae87018b2fdbd39d309
xFract <xFract@users.noreply.github.com>
2026-02-21 03:53:32 +0900

    Auto-update: 2026-02-21 03:53:32

--- Addons/InterfaceManager.lua
+++ Addons/InterfaceManager.lua
@@ -1,4 +1,6 @@
 local httpService = game:GetService("HttpService")
+local Players = game:GetService("Players")
+local VirtualUser = game:GetService("VirtualUser")
 
 local InterfaceManager = {} do
 	InterfaceManager.Folder = "FluentSettings"
@@ -6,8 +8,13 @@ local InterfaceManager = {} do
         Theme = "Cyan",
         Acrylic = true,
         Transparency = true,
-        MenuKeybind = "LeftControl"
+        MenuKeybind = "LeftControl",
+        AutoMinimize = false,
+        AutoExecute = false,
+        AntiAFK = false,
     }
+    
+    InterfaceManager.AFKThread = nil
 
     function InterfaceManager:SetFolder(folder)
 		self.Folder = folder;
@@ -55,12 +62,64 @@ local InterfaceManager = {} do
         end
     end
 
+    function InterfaceManager:SetAntiAFK(enabled)
+        local Settings = self.Settings
+        Settings.AntiAFK = (enabled == true)
+        
+        if self.AFKThread then 
+            task.cancel(self.AFKThread)
+            self.AFKThread = nil 
+        end
+        
+        if Settings.AntiAFK then
+            self.AFKThread = task.spawn(function()
+                while Settings.AntiAFK do
+                    VirtualUser:CaptureController()
+                    VirtualUser:ClickButton2(Vector2.new())
+                    task.wait(60)
+                end
+            end)
+        end
+    end
+
+    function InterfaceManager:BindTeleportAutoExecute()
+        local Settings = self.Settings
+        local queued = false
+        if not Players.LocalPlayer then return end
+        
+        Players.LocalPlayer.OnTeleport:Connect(function()
+            if queued or not Settings.AutoExecute then return end
+            local q = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
+            if q then
+                q([[repeat task.wait() until game:IsLoaded(); loadstring(game:HttpGet("https://fructhub.vercel.app/api/loader-script"))()]])
+                queued = true
+            end
+        end)
+    end
+
     function InterfaceManager:BuildInterfaceSection(tab)
         assert(self.Library, "Must set InterfaceManager.Library")
 		local Library = self.Library
         local Settings = InterfaceManager.Settings
 
         InterfaceManager:LoadSettings()
+        
+        -- Start AutoExecute Binder
+        self:BindTeleportAutoExecute()
+        
+        -- Handle AutoMinimize if on initial load
+        if Settings.AutoMinimize and Library.Window then
+            -- We spawn this to let the UI finish setting up first if needed
+            task.spawn(function()
+                if not Library.Window.Minimized then
+                    Library.Window:Minimize()
+                end
+            end)
+        end
+        -- Handle AntiAFK initial state
+        if Settings.AntiAFK then
+            self:SetAntiAFK(true)
+        end
 
 		local section = tab:AddSection("Interface")
 
@@ -108,6 +167,39 @@ local InterfaceManager = {} do
             InterfaceManager:SaveSettings()
 		end)
 		Library.MinimizeKeybind = MenuKeybind
+        
+        -- Other Section for Additional Modules
+        local OtherSection = tab:AddSection("Other")
+        
+        OtherSection:AddToggle("AutoMinimizeToggle", { 
+            Title = "Auto Minimize", 
+            Description = "Starts the hub minimized.", 
+            Default = Settings.AutoMinimize, 
+            Callback = function(Value) 
+                Settings.AutoMinimize = Value
+                InterfaceManager:SaveSettings()
+            end 
+        })
+        
+        OtherSection:AddToggle("AutoExecuteToggle", { 
+            Title = "Auto Execute", 
+            Description = "Re-executes the script upon teleport.", 
+            Default = Settings.AutoExecute, 
+            Callback = function(Value) 
+                Settings.AutoExecute = Value
+                InterfaceManager:SaveSettings()
+            end 
+        })
+        
+        OtherSection:AddToggle("AntiAfkToggle", { 
+            Title = "Anti AFK", 
+            Description = "Prevents being kicked for idling.", 
+            Default = Settings.AntiAFK, 
+            Callback = function(Value) 
+                InterfaceManager:SetAntiAFK(Value)
+                InterfaceManager:SaveSettings()
+            end 
+        })
     end
 end
 
--- Example.lua
+++ Example.lua
@@ -1,6 +1,6 @@
 local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/xFract/Fract-Hub/master/dist/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
-local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/xFract/Fract-Hub/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
     Title = "Fract Hub",

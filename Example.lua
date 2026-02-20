3fe8208e73fb6857093a43de1da42b855f443733
xFract <adomensdo@gmail.com>
2026-02-20 19:39:10 +0900

    mini Update Example.lua

--- Example.lua
+++ Example.lua
@@ -3,7 +3,7 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Title = "Maru Hub",
+    Title = "Fract Hub",
     SubTitle = "Solo Hunter",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
